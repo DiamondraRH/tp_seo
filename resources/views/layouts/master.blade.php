4bd73af883326b46839580a580ac8117972db684
DiamondraRH <rambelosondidy@gmail.com>
2023-03-23 13:08:11 +0300

    meta viewport

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -18,6 +18,9 @@
     <link rel="stylesheet" href="../assets/css/style.css" >
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <meta name="description" content="@yield('metadata')"/>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
     <header class="header-top bg-grey justify-content-center">
