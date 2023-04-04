a147e7f5cd17597d23e1c896f0522f383ee2b3de
DiamondraRH <rambelosondidy@gmail.com>
2023-04-04 15:12:57 +0300

    fix middleware error

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
+use App\Http\Middleware\GzipMiddleware;
 
 /*
 |--------------------------------------------------------------------------
