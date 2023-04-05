8138a04301d87f97c7cd781d9ad63a43e49f1d39
DiamondraRH <rambelosondidy@gmail.com>
2023-04-05 11:32:43 +0300

    revert test cache

--- resources/views/article-list.blade.php
+++ resources/views/article-list.blade.php
@@ -6,6 +6,7 @@
             <div class="row">
                 <div class="col-sm-12">
                     <div class="text-center">
+                        <h2 class="lg-title">Articles</h2>
                     </div>
                 </div>
             </div>
