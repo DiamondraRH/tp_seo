54ba558b619e568c7997b13e231358736961923c
DiamondraRH <rambelosondidy@gmail.com>
2023-04-05 11:28:20 +0300

    test cache

--- resources/views/article-list.blade.php
+++ resources/views/article-list.blade.php
@@ -6,7 +6,6 @@
             <div class="row">
                 <div class="col-sm-12">
                     <div class="text-center">
-                        <h2 class="lg-title">Articles</h2>
                     </div>
                 </div>
             </div>
